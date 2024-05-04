776ff190cb00fae175048ef0f52d90f353564d65
cfking24 <77686459+cfking24@users.noreply.github.com>
2024-05-04 08:59:36 -0700

    updated initialTest and added images

--- initialTest.py
+++ initialTest.py
@@ -4,7 +4,8 @@ import cv2
 # Example usage
 if __name__ == '__main__':
     # Load your input image (replace with your actual image path)
-    input_image_path = 'path/to/your/input/image.jpg'
+    input_image_path = 'C:\\Users\\Conor King\\Documents\\School\\EEC 289A\\HW 2 - 2nd Attempt\\TextureSynthesis\\examples\\161.jpg'
+    
     sample = cv2.imread(input_image_path)
 
     # Display the image
@@ -16,14 +17,26 @@ if __name__ == '__main__':
     window_height, window_width = 50, 50
     kernel_size = 11
 
-    # Initialize texture synthesis
-    (sample, window, mask, padded_window,
-        padded_mask, result_window) = initialize_texture_synthesis(sample, (window_height, window_width), kernel_size)
+    # # Assuming 'original_sample' is loaded correctly
+    # # Check if the image has an alpha channel (RGBA format)
+    # if sample.shape[2] == 4:
+    #     # Remove the alpha channel
+    #     original_sample = sample[:, :, :3]
 
+    # # Convert to grayscale
+    # greysample = cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)
+
+
+    # Initialize texture synthesis
+    # (sample, window, mask, padded_window,
+    #     padded_mask, result_window) = initialize_texture_synthesis(sample, (window_height, window_width), kernel_size)
+
+    visualize = True
     # Perform texture synthesis
-    synthesized_texture = synthesize_texture(sample, (window_height, window_width), kernel_size)
+    synthesized_texture = synthesize_texture(sample, (window_height, window_width), kernel_size, visualize=visualize)
 
     # Save the synthesized texture (replace with your desired output path)
-    output_image_path = 'path/to/your/output/synthesized_texture.jpg'
+    output_image_path = 'C:\\Users\\Conor King\\Documents\\School\\EEC 289A\\HW 2 - 2nd Attempt\\TextureSynthesis\\Synthesized Images\\1.png'
+    sample = cv2.imread(input_image_path)
     cv2.imwrite(output_image_path, synthesized_texture)
     print(f"Synthesized texture saved to {output_image_path}")
